9cd76eeb8318d129a90a293046f4947fa6ebab87
Dmndz <barbara@dsplce.co>
2025-12-06 18:56:47 +0100

    feat: add bindings requisite for client initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::Serialize;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[derive(Clone)]
+    #[wasm_bindgen(js_namespace = Statsig, js_name = StatsigClient)]
+    pub type StatsigClient;
+
+    #[wasm_bindgen(constructor, js_namespace = Statsig)]
+    pub fn _new(api_key: &str, options: &JsValue) -> StatsigClient;
+
+    #[wasm_bindgen(method, js_namespace = Statsig, js_name = initializeAsync)]
+    pub async fn initialize_async(this: &StatsigClient);
+
+    #[wasm_bindgen(method, js_namespace = Statsig, js_name = initializeSync)]
+    pub fn initialize_sync(this: &StatsigClient);
+
+    #[wasm_bindgen(js_namespace = Statsig, js_name = runStatsigAutoCapture)]
+    pub fn run_statsig_auto_capture(client: &StatsigClient);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize)]
+pub struct StatsigClientOptions {
+    #[serde(rename = "userID")]
+    pub user_id: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl StatsigClient {
+    pub fn new(api_key: &str, options: StatsigClientOptions) -> Result<StatsigClient, JsValue> {
+        let js_options = serde_wasm_bindgen::to_value(&options)
+            .map_err(|e| JsValue::from_str(&format!("Failed to serialize options: {}", e)))?;
+
+        Ok(StatsigClient::_new(api_key, &js_options))
     }
 }
