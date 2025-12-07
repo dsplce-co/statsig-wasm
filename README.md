d33b14b28402f5f99f0b551d464fb35b7825d435
Dmndz <barbara@dsplce.co>
2025-12-07 14:29:57 +0100

    feat: add readme

---
+++ README.md
@@ -0,0 +1,74 @@
+> We're dsplce.co, check out our work on [github.com/dsplce-co](https://github.com/dsplce-co) 🖤
+
+# statsig-wasm
+
+📈 Wasm bindings for the Statsig JavaScript (Web) SDK.
+
+---
+
+## 📦 Installation
+
+In your project, run:
+
+```sh
+cargo add statsig-wasm
+```
+
+## 🔧 Setup
+
+Add the following script to your HTML `<head>` tag to make Statsig available to your Wasm module:
+
+```html
+<script
+  src="https://cdn.jsdelivr.net/npm/@statsig/js-client@3/build/statsig-js-client+session-replay+web-analytics.min.js"
+  crossorigin="anonymous"
+>
+</script>
+```
+
+Bear in mind, if you're happy for Statsig to autoinitialise the client, just append `?apikey=<YOUR_CLIENT_API_KEY>` to the script's `src` above and uninstall this crate 😉
+
+If you're looking to initialise the client yourself, read on.
+
+---
+
+## 🧪 Usage
+
+Set a custom user ID and initialise the client:
+
+```rust
+use statsig_wasm::{StatsigClient, StatsigUser};
+
+let statsig = StatsigClient::new(
+    env!("STATSIG_API_KEY"),
+    StatsigUser {
+        user_id: user_id.get(),
+    },
+)
+.unwrap();
+
+run_statsig_auto_capture(&statsig);
+
+// `spawn_local` is native to Leptos, use your
+// framework's equivalent to run the async method.
+spawn_local(async move {
+    statsig.initialize().await;
+});
+```
+
+---
+
+## 📁 Repo & Contributions
+
+📦 Crate: [crates.io/crates/statsig-wasm](https://crates.io/crates/statsig-wasm)<br>
+🛠️ Repo: [github.com/dsplce-co/statsig-wasm](https://github.com/dsplce-co/statsig-wasm)
+
+PRs welcome! 🖤
+
+---
+
+## 🔒 License
+
+MIT or Apache-2.0, at your option.
+
+---
--- src/lib.rs
+++ src/lib.rs
@@ -8,29 +8,32 @@ extern "C" {
     pub type StatsigClient;
 
     #[wasm_bindgen(constructor, js_namespace = Statsig)]
-    pub fn _new(api_key: &str, options: &JsValue) -> StatsigClient;
+    pub fn _new(api_key: &str, user: &JsValue) -> StatsigClient;
 
     #[wasm_bindgen(method, js_namespace = Statsig, js_name = initializeAsync)]
-    pub async fn initialize_async(this: &StatsigClient);
+    pub async fn initialize(this: &StatsigClient);
 
     #[wasm_bindgen(method, js_namespace = Statsig, js_name = initializeSync)]
     pub fn initialize_sync(this: &StatsigClient);
+
+    #[wasm_bindgen(js_namespace = Statsig, js_name = runStatsigSessionReplay)]
+    pub fn run_statsig_session_replay(client: &StatsigClient);
 
     #[wasm_bindgen(js_namespace = Statsig, js_name = runStatsigAutoCapture)]
     pub fn run_statsig_auto_capture(client: &StatsigClient);
 }
 
 #[derive(Serialize)]
-pub struct StatsigClientOptions {
+pub struct StatsigUser {
     #[serde(rename = "userID")]
     pub user_id: String,
 }
 
 impl StatsigClient {
-    pub fn new(api_key: &str, options: StatsigClientOptions) -> Result<StatsigClient, JsValue> {
-        let js_options = serde_wasm_bindgen::to_value(&options)
-            .map_err(|e| JsValue::from_str(&format!("Failed to serialize options: {}", e)))?;
+    pub fn new(api_key: &str, user: StatsigUser) -> Result<StatsigClient, String> {
+        let js_user = serde_wasm_bindgen::to_value(&user)
+            .map_err(|e| format!("Failed to serialize user: {}", e))?;
 
-        Ok(StatsigClient::_new(api_key, &js_options))
+        Ok(StatsigClient::_new(api_key, &js_user))
     }
 }
